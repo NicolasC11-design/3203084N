bcf485f142ea7aa1d905ca4a7d5ef9eee6b2dfc3
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2025-08-04 22:14:11 -0500

    ejercicios

---
+++ Modulos/Modulo2/ejercicios/Conjuntos.py
@@ -0,0 +1,8 @@
+set1 = {100, 200, 300}
+set2 = {300, 10, 20, 300}
+set3 = set1.union(set2)
+set4 = set1.intersection(set2)
+set5 = set2.difference(set1)
+print(set3)
+print(set4)
+print(set5)
---
+++ Modulos/Modulo2/ejercicios/Diccionario.py
@@ -0,0 +1,4 @@
+dict= {"Nombre": "Nicolas", "Edad": 17, "Carrera": "Futbolista"}
+dict['clave']="Institucion:Colombia"
+print(dict["Nombre"])
+print(dict)
