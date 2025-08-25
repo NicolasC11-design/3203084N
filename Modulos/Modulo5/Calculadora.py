1cab4ddeff645d1446aebecb47dc240ad35c6337
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2025-08-24 21:33:08 -0500

    Add files via upload

---
+++ Modulos/Modulo5/Calculadora.py
@@ -0,0 +1,11 @@
+print("****************************************") 
+int_a = input("Digite el primer número: ")
+int_b = input("Digite el segundo número: ")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+print(a/b)
+
+print("****************************************")
+
